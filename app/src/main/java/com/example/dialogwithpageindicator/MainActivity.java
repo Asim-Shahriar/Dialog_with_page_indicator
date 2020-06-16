820d3eb0cfed2e3dd8a2bf1c275cc3c7bbc6d2d7
Asim-Shahriar <asimshariar@gmail.com>
2020-06-16 23:43:02 +0600

    Mainactivity dialogshowing code added

--- app/src/main/java/com/example/dialogwithpageindicator/MainActivity.java
+++ app/src/main/java/com/example/dialogwithpageindicator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dialogwithpageindicator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -8,42 +9,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ViewPager2 viewPager=findViewById(R.id.pager);
-        viewPager.setAdapter(createAdapter());
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-                if(position ==0){
-
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
-            }
-        });
-
+        showDialog();
     }
 
-    private ViewPagerLatest createAdapter() {
-
-       ViewPagerLatest adapter=new ViewPagerLatest(this);
-       return adapter;
+    private void showDialog() {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        PageIndicatorFragment pageIndicatorFragment=new PageIndicatorFragment();
+        pageIndicatorFragment.show(fragmentManager,"viewpager2");
     }
-
-
 }
